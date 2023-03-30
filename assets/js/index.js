6184f5ad8d3a0186fb89987caf99ddc7f569fcba
nozaki08 <uryahoooi89@gmail.com>
2023-03-30 15:22:04 +0900

    max-widoth入れます。
    Maxwitdht入れます。
    
    野崎

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,8 +8,8 @@ $(".kv_slide").slick({
 });
 
 // slick(pickupスライド)
-$(".").slick({
-    autoplay: true, // 自動再生
-    autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
-    infinite: true, // 無限スライド
-});
+// $(".").slick({
+//     autoplay: true, // 自動再生
+//     autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
+//     infinite: true, // 無限スライド
+// });
