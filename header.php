0702fad895ad48aa9fb88ac1070642567458b085
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-03-30 17:36:18 +0900

    CSSを本番用に繋ぎ直しています
    CSSを本番用に繋ぎ直しています
    
    三宅

--- header.php
+++ header.php
@@ -11,26 +11,31 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <header class="header">
-        <div class="header_inner">
+        <div class="header_inner flex">
             <div class="header_logo">
-                <h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/logo@2x.png" alt="徳島こども食堂ナビ"></a></h1>
+                <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ロゴ（仮）.png" alt="ホームボタン"></a>
             </div>
-            <div class="header_links">
-                <nav class="gnav">
-                    <?php
+
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <!-- hamburger箱end -->
+            <nav class="menu_pc">
+                <?php
                         $args = array(
                         'menu' => 'global-navigation',  //管理画面で作成したメニューの名前
-                        'menu_class' => '', //メニューを構成するulタグのクラス名
+                        'menu_class' => 'menu_pc_ul flex', //メニューを構成するulタグのクラス名
                         'container' => false, //<ul>タグを囲んでいる<div>タグを削除
                         );
                         wp_nav_menu($args);
                         ?>
-                </nav>
-                <form action="<?php echo home_url('/'); ?>" method="get" class="header_search">
-                    <input type="text" name="s" value="<?php the_search_query(); ?>" placeholder="キーワードを入力">
-                    <i class="fas fa-search"></i>
-                </form>
-            </div>
+            </nav>
+            <form action="<?php echo home_url('/'); ?>" method="get" class="header_search">
+                <input type="text" name="s" value="<?php the_search_query(); ?>" placeholder="キーワードを入力">
+                <i class="fas fa-search"></i>
+            </form>
 
         </div>
 
