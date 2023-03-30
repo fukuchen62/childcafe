7b449551d20dba935934c7fd08df761fee83aac4
nozaki08 <uryahoooi89@gmail.com>
2023-03-30 19:13:18 +0900

    活動の様子を共有します
    活動の様子を共有します。
    
    野崎

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
 "use strict";
 
 // slick(KVスライド)
-$(".kv_slide").slick({
+$(".kv_slider").slick({
     autoplay: true, // 自動再生
     autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
     infinite: true, // 無限スライド
 });
 
 // slick(pickupスライド)
-// $(".").slick({
-//     autoplay: true, // 自動再生
-//     autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
-//     infinite: true, // 無限スライド
-// });
+$(".pickup_slider").slick({
+    autoplay: true, // 自動再生
+    autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
+    infinite: true, // 無限スライド
+});
+
+// slick(活動の様子スライド)
+$(function () {
+    $(".activity_slide").slick({
+        arrows: false,
+        autoplay: true,
+        adaptiveHeight: true,
+        centerMode: true,
+        centerPadding: "8%",
+        dots: true,
+        slidesToShow: 4,
+    });
+});
