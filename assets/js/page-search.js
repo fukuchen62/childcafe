549960d9709ea8c22a2b54cab2e4ca02555dba12
yshizuka <128563427+yshizuka@users.noreply.github.com>
2023-03-31 09:23:04 +0900

    プルダウン複数選択テスト
    プルダウン複数選択テスト中
    
    吉田

---
+++ assets/js/page-search.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(function () {
+    $("select").multipleSelect({
+        width: 200,
+        formatSelectAll: function () {
+            return "すべて";
+        },
+        formatAllSelected: function () {
+            return "全て選択されています";
+        },
+    });
+});
