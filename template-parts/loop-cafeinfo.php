4ff5e3a55e1aefcd835cf62a4fc7414f22ced14f
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-01 19:59:02 +0900

    loop-cafeinfo.phpを変更しました
    loop-cafeinfo.phpを変更しました
    
    三宅

--- template-parts/loop-cafeinfo.php
+++ template-parts/loop-cafeinfo.php
@@ -1,8 +1,14 @@
 <div class="result_img_wrap flex">
     <a href="<?php the_permalink(); ?>">
         <article class="result_img_card">
+            <?php $eye_catching = get_post_meta($post->ID, 'eye_catching', true);?>
+            <?php if(!empty($eye_catching)): ?>
             <img src="<?php the_field('eye_catching'); ?>" alt="">
-            <p><?php the_field('name'); ?></p>
+            <?php else: ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/text_kakko_kari.png" alt="">
+            <?php endif; ?>
+            <p>
+                <?php the_field('name'); ?></p>
             <p>
                 <?php
                     $this_terms = get_the_terms($post->ID,'area');
