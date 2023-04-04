b84d4282f2d9c486181e614373eee6fbb807069e
yshizuka <128563427+yshizuka@users.noreply.github.com>
2023-04-04 13:59:54 +0900

    トップページへ戻るボタン作成
    トップページへ戻るボタン作成
    
    吉田

--- assets/js/header.js
+++ assets/js/header.js
@@ -12,3 +12,20 @@ $(".menu a").on("click", function () {
     $(".hamburger").removeClass("is-active");
     $(".menu").removeClass("is-active");
 });
+
+// トップボタンスクロールアニメーション
+$(".toppage_btn").on("click", function () {
+    $("window,html").animate({ scrollTop: 0 }, 550);
+});
+
+// トップボタン非表示
+$(".toppage_btn").hide();
+// トップボタン表示条件
+$(window).on("scroll", function () {
+    let value = $(this).scrollTop();
+    if (value > 80) {
+        $(".toppage_btn").fadeIn();
+    } else {
+        $(".toppage_btn").fadeOut();
+    }
+});
