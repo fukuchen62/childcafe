ad3d9ed63ab27469b145b222e1411c85a3f43ca7
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-05 13:50:33 +0900

    途中経過
    途中経過
    
    三宅

--- archive-event.php
+++ archive-event.php
@@ -1,5 +1,43 @@
 <?php get_header(); ?>
+<?php get_template_part('template-parts/breadcrumb'); ?>
+<?php
 
-<h2>開催予定</h2>
+$args = array(
+		'post_type' => 'event',
+        // 'meta_key' => 'class',
+        // //不定期のもの
+        // 'meta_value' => 2,
+		'posts_per_page' => -1,
+        'paged' => get_query_var('paged') //何ページ目の情報を表示すれば良いか
+	);
+$the_query = new WP_Query($args);
+?>
+
+<main>
+    <div class="main_inner">
+        <h2 class="title">開催情報一覧</h2>
+        <?php if ($the_query->have_posts()) : ?>
+        <?php while($the_query->have_posts()) : ?>
+        <?php $the_query->the_post(); ?>
+        <div class="ivent_item ivent_flex">
+            <div class="ivent_item_card">
+                <a href="<?php the_permalink(); ?>">
+                    <img src="<?php the_field('eye_catching'); ?>" alt="pickup画像" />
+                    <p>3月1日　徳島市</p>
+                    <p>●●食堂　開催のお知らせ</p>
+                </a>
+            </div>
+        </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
+        <?php wp_reset_postdata(); ?>
+        <?php
+            if (function_exists('wp_pagenavi')) {
+                wp_pagenavi();
+                }
+            ?>
+        <?php get_sidebar('categories'); ?>
+    </div>
+</main>
 
 <?php get_footer(); ?>
