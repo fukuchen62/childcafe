fe6b36e3042c2142527cbf9158d5b9910e156418
yshizuka <128563427+yshizuka@users.noreply.github.com>
2023-04-05 13:49:34 +0900

    ｐｈｐ化
    ｐｈｐ化
    
    吉田

--- footer.php
+++ footer.php
@@ -1,11 +1,19 @@
 <footer class="footer">
-    <!-- 波型の画像 -->
-    <img class="footer_wave" src="<?php echo get_template_directory_uri(); ?>/assets/images/footer_wave.svg" alt="" />
-    <div class="footer_inner">
-        <nav class="footer_container with_girl">
-            <!-- 女の子の画像 -->
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/girl.png" alt="" class="footer_girl" />
-            <?php
+            <!-- 波型の画像 -->
+            <!-- <img
+                class="footer_wave"
+                src="../assets/images/footer_wave.svg"
+                alt=""
+            /> -->
+            <div class="footer_inner">
+                <nav class="footer_container with_girl">
+                    <!-- 女の子の画像 -->
+                    <img
+                        src="../assets/images/girl.png"
+                        alt=""
+                        class="footer_girl"
+                    />
+                    <?php
             $hoge = array(
             'menu' => 'footer-navigation',  //管理画面で作成したメニューの名前
             'menu_class' => 'footer_nav flex', //メニューを構成するulタグのクラス名
@@ -13,16 +21,14 @@
             );
             wp_nav_menu($hoge);
             ?>
-            <!-- footer_nav end -->
-        </nav>
-        <div class="footer_copyright">
-            <p>copyright©</p>
-        </div>
-    </div>
-    <!-- footer_inner end -->
-</footer>
-</div>
-<!-- ボディーインナー終わり -->
-<?php wp_footer(); ?>
-</body>
+                    <!-- footer_nav end -->
+                    <div class="footer_copyright">
+                        <p>copyright©</p>
+                    </div>
+                </nav>
+            </div>
+            <!-- footer_inner end -->
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
 </html>
