34874d42e99d8e1c280220ff564cbbe73551cb7d
yshizuka <128563427+yshizuka@users.noreply.github.com>
2023-04-09 16:18:31 +0900

    img接続
    img接続
    
    よしだ

--- page-program.php
+++ page-program.php
@@ -2,33 +2,37 @@
 <?php get_template_part('template-parts/breadcrumb'); ?>
 
 <main>
-                <div class="main_inner">
-                    <h2 class="title">
-                        <span class="title sp">みんなで応援</span>
-                        <span class="title sp">応援プログラムとは</span>
-                    </h2>
-                    <div class="text">
-                        <p>
-                            物資の支援を必要としている団体がAmazon「ほしい物リスト」を作成し、公開することで、支援したい人が遠隔地にいながら、記名または匿名で支援が行える制度です。
-                        </p>
-                    </div>
-                    <div class="text">
-                        <p>
-                            こども食堂を運営するには、食材だけでなく、料理関連や衛生関連の消耗品等が必要で、ボランティアスタッフが持ち出しで購入している食堂も多くあります。
-                        </p>
-                    </div>
-                    <div class="text">
-                        <p>
-                            ここでは、Amazonみんなで応援プログラムに登録しているこども食堂と、各こども食堂のAmazonみんなで応援プログラムURLを掲載しています。
-                        </p>
-                    </div>
-                    <div class="text">
-                        <p>ご支援を宜しくお願いいたします。</p>
-                    </div>
-                    <a href="<?php echo home_url('programlist'); ?>">
-                        <div class="btn program_btn">参加食堂一覧へ</div>
-                    </a>
-                </div>
-            </main>
+    <div class="main_inner">
+        <h2 class="amazon_title">
+            <span class="title sp">Amazonみんなで</span>
+            <span class="title sp">応援プログラムとは</span>
+        </h2>
+        <div class="text">
+            <p>
+                物資の支援を必要としている団体がAmazon「ほしい物リスト」を作成し、公開することで、支援したい人が遠隔地にいながら、記名または匿名で支援が行える制度です。
+            </p>
+        </div>
+        <div class="text">
+            <p>
+                こども食堂を運営するには、食材だけでなく、料理関連や衛生関連の消耗品等が必要で、ボランティアスタッフが持ち出しで購入している食堂も多くあります。
+            </p>
+        </div>
+        <div class="text">
+            <p>
+                ここでは、Amazonみんなで応援プログラムに登録しているこども食堂と、各こども食堂のAmazonみんなで応援プログラムURLを掲載しています。
+            </p>
+        </div>
+        <div class="text">
+            <p>ご支援を宜しくお願いいたします。</p>
+        </div>
+
+        <div class="btn program_btn"><a href="<?php echo home_url('programlist'); ?>">参加食堂一覧へ</a></div>
+
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/amazon/vegetable.png" alt="背景の野菜"
+            class="amazon_img vegetable" />
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/amazon/family.png" alt="背景の親子"
+            class="amazon_img family" />
+    </div>
+</main>
 
 <?php get_footer(); ?>
