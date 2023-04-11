c091ca014d4111700a8a27b3bbb850205f302344
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-11 18:57:41 +0900

    taxonomy-link.phpを変更しました
    taxonomy-link.phpを変更しました
    
    三宅

--- template-parts/breadcrumb.php
+++ template-parts/breadcrumb.php
@@ -1,9 +1,12 @@
 <div class="breadcrumb">
     <div class="breadcrumb_inner">
+
         <?php
         if (function_exists('bcn_display')) {
-            bcn_display();
+            if (!wp_is_mobile()) {
+                bcn_display();
         }
+    }
         ?>
     </div>
 </div>
