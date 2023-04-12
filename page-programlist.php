f55de92719b54e05d43dc83d0765dc466e220130
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-12 17:20:42 +0900

    page-programlist.phpを変更しました
    page-programlist.phpを変更しました
    
    三宅

--- page-programlist.php
+++ page-programlist.php
@@ -2,50 +2,70 @@
 <?php get_template_part('template-parts/breadcrumb'); ?>
 
 <?php
+// $args = array(
+// 		'post_type' => 'cafeinfo',
+//         'meta_type' => 'amapro',
+// 		'posts_per_page' => -1,
+//         'paged' => get_query_var('paged') //何ページ目の情報を表示すれば良いか
+// 	);
+// $the_query = new WP_Query($args);
+?>
+
+<?php
 $args = array(
 		'post_type' => 'cafeinfo',
-        'meta_type' => 'amapro',
-		'posts_per_page' => -1,
-        'paged' => get_query_var('paged') //何ページ目の情報を表示すれば良いか
-	);
+        'posts_per_page' => 6,
+        'paged' => get_query_var('paged'), //何ページ目の情報を表示すれば良いか
+        'meta_query' => array(
+            array(
+                'key' => 'amapro',
+                'value' => 'http',
+                'compare' => 'LIKE',
+            )
+            )
+            );
+
 $the_query = new WP_Query($args);
 ?>
 
+
+
 <!-- 参加食堂一覧 -->
-            <main>
-                <div class="main_inner">
-                    <div class="result_img">
-                        <h2 class="title">参加こども食堂一覧</h2>
-                        <div class="result_img_wrap flex">
-                            <?php if ($the_query->have_posts()) : ?>
-                                <?php while($the_query->have_posts()) : ?>
-                                <?php $the_query->the_post(); ?>
-                                <?php if(! empty(get_field('amapro'))): ?>
-                            <a href="<?php the_permalink(); ?>">
-                                <div class="result_img_card">
+<main>
+    <div class="main_inner">
+        <div class="result_img">
+            <h2 class="title">参加こども食堂一覧</h2>
+            <div class="result_img_wrap flex">
+                <?php if ($the_query->have_posts()) : ?>
+                <?php while($the_query->have_posts()) : ?>
+                <?php $the_query->the_post(); ?>
 
-                                <?php $eye_catching = get_field('eye_catching');?>
-                                <?php if(!empty($eye_catching)): ?>
-                                    <img src="<?php the_field('eye_catching'); ?>" alt="">
-                                <?php else: ?>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/text_kakko_kari.png" alt="">
-                                <?php endif; ?>
+                <?php //if(! empty(get_field('amapro'))): ?>
 
-                                <p><?php the_field('name'); ?></p>
-                                <?php $this_terms = get_the_terms($post->ID,'area'); ?>
-                                <p><?php echo '(' . $this_terms[1]-> name . ')' ; ?></p>
-                            </div>
-                            </a>
-                            <?php endif ?>
-                            <?php endwhile; ?>
-                                <?php endif ?>
-                                <?php wp_reset_postdata(); ?>
+                <a href="<?php the_permalink(); ?>">
+                    <div class="result_img_card">
 
+                        <?php $eye_catching = get_field('eye_catching');?>
+                        <?php if(!empty($eye_catching)): ?>
+                        <img src="<?php the_field('eye_catching'); ?>" alt="">
+                        <?php else: ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/text_kakko_kari.png" alt="">
+                        <?php endif; ?>
 
-                        </div>
+                        <p><?php the_field('name'); ?></p>
+                        <?php $this_terms = get_the_terms($post->ID,'area'); ?>
+                        <p><?php echo '(' . $this_terms[1]-> name . ')' ; ?></p>
                     </div>
-                </div>
-            </main>
-            <!-- 参加食堂一覧 終了-->
+                </a>
+                <?php //endif ?>
+
+                <?php endwhile; ?>
+                <?php endif ?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- 参加食堂一覧 終了-->
 
 <?php get_footer(); ?>
