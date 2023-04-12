31030d288abb47be888c14cd6fcf31b263f1c3e2
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-12 15:56:48 +0900

    page-post.phpを編集しました
    page-post.phpを編集しました
    
    三宅

--- page-post.php
+++ page-post.php
@@ -25,16 +25,40 @@ $the_query = new WP_Query($args);
                 <?php endif; ?>
                 <?php wp_reset_postdata(); ?>
             </div>
+            <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            ?>
+            <div class="page_nav flex pc_none">
+                <?php
+                global $wp_rewrite;
+                $paginate_base = get_pagenum_link(1);
+                if(strpos($paginate_base, '?') || !$wp_rewrite->using_permalinks()){
+                $paginate_format = '';
+                $paginate_base = add_query_arg('paged','%#%');
+                }else{
+                $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+                user_trailingslashit('page/%#%/','paged');
+                $paginate_base .= '%_%';
+                }
+                echo paginate_links(array(
+                'base' => $paginate_base,
+                'format' => $paginate_format,
+                'total' => $the_query->max_num_pages,
+                'mid_size' => 1,
+                'current' => ($paged ? $paged : 1),
+                'prev_text' => '<div class="page_triangle_left"></div>',
+                'next_text' => '<div class="page_triangle_right"></div>',
+                ));
+            ?>
+            </div>
             <?php get_sidebar('categories'); ?>
+
         </div>
         <!-- ページナビ -->
-        <?php original_pagenation(); ?>
+        <?php //original_pagenation(); ?>
 
 
-        <?php
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        ?>
-        <div class="page_nav flex">
+        <div class="page_nav flex sp_none">
             <?php
                 global $wp_rewrite;
                 $paginate_base = get_pagenum_link(1);
