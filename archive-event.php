a9b46861a2b3c1c946c5d86918ce9143fd08d846
nozaki08 <uryahoooi89@gmail.com>
2023-04-12 19:21:08 +0900

    Update archive-event.php

--- archive-event.php
+++ archive-event.php
@@ -52,23 +52,55 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
             <?php endwhile; ?>
             <?php endif; ?>
             <?php wp_reset_postdata(); ?>
-        </div>
-        <!-- ページナビ -->
-        <div class="page_nav flex">
-            <?php original_pagenation(); ?>
-        </div>
-        <style>
-        .page-numbers {
-            width: 37px;
-            height: 37px;
-            padding-top: 3px;
-            background-color: #f7dd94;
-            border-radius: 50px;
-            text-align: center;
-        }
-        </style>
-        <?php get_sidebar('categories'); ?>
-    </div>
+
+            <div class="page_nav flex pc_none">
+                <?php
+                global $wp_rewrite;
+                $paginate_base = get_pagenum_link(1);
+                if(strpos($paginate_base, '?') || !$wp_rewrite->using_permalinks()){
+                $paginate_format = '';
+                $paginate_base = add_query_arg('paged','%#%');
+                }else{
+                $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+                user_trailingslashit('page/%#%/','paged');
+                $paginate_base .= '%_%';
+                }
+                echo paginate_links(array(
+                'base' => $paginate_base,
+                'format' => $paginate_format,
+                'total' => $the_query->max_num_pages,
+                'mid_size' => 1,
+                'current' => ($paged ? $paged : 1),
+                'prev_text' => '<div class="page_triangle_left"></div>',
+                'next_text' => '<div class="page_triangle_right"></div>',
+                ));
+            ?>
+
+                <?php get_sidebar('categories'); ?>
+            </div>
+            <div class="page_nav flex sp_none">
+                <?php
+                global $wp_rewrite;
+                $paginate_base = get_pagenum_link(1);
+                if(strpos($paginate_base, '?') || !$wp_rewrite->using_permalinks()){
+                $paginate_format = '';
+                $paginate_base = add_query_arg('paged','%#%');
+                }else{
+                $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+                user_trailingslashit('page/%#%/','paged');
+                $paginate_base .= '%_%';
+                }
+                echo paginate_links(array(
+                'base' => $paginate_base,
+                'format' => $paginate_format,
+                'total' => $the_query->max_num_pages,
+                'mid_size' => 1,
+                'current' => ($paged ? $paged : 1),
+                'prev_text' => '<div class="page_triangle_left"></div>',
+                'next_text' => '<div class="page_triangle_right"></div>',
+                ));
+            ?>
+            </div>
 </main>
 </div>
 
