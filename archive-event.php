76587c00fb029ceff8322d6cdf00843e9b915eeb
nozaki08 <uryahoooi89@gmail.com>
2023-04-12 19:09:12 +0900

    eventphp　更新
    event更新済
    
    野崎

--- archive-event.php
+++ archive-event.php
@@ -20,7 +20,7 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
     <div class="main_inner">
         <?php get_template_part('template-parts/breadcrumb'); ?>
         <h2 class="title">開催情報一覧</h2>
-        <div class="ivent_item ivent_flex">
+        <div class="event_flex">
             <?php if ($the_query->have_posts()) : ?>
             <?php while($the_query->have_posts()) : ?>
             <?php $the_query->the_post(); ?>
@@ -28,15 +28,20 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
             $cafeinfo_id = get_field('id');
             $this_terms = get_the_terms($cafeinfo_id,'area');
             ?>
-            <div class="ivent_item_card">
+            <div class="event_item">
                 <a href="<?php the_permalink(); ?>">
                     <img src="<?php the_field('eye_catching'); ?>" alt="pickup画像" />
-                    <p>
+                    <p class="event_item_card_title">
                         <!-- 日付表示加工予定 ◯月◯日-->
                         <?php echo get_field('datetime').'　'.$this_terms[1]->name; ?>
                     </p>
-                    <p>
-                        <?php echo get_field('name',$cafeinfo_id).'　開催のお知らせ'; ?>
+                    <p class="event_item_card_title border">
+                        <?php echo get_field('name',$cafeinfo_id); ?>
+                    </p>
+                    <p class="event_text">
+                        <?php if (!empty(get_field('appeal'))): ?>
+                        <?php echo get_field('appeal'); ?>
+                        <?php endif; ?>
                     </p>
                 </a>
             </div>
