b6073a9366aef4b8f85538097669c82cafc98abc
nozaki08 <uryahoooi89@gmail.com>
2023-04-13 17:21:54 +0900

    page-find.php修正
    page-find.php修正済です。
    
    野崎

--- page-find.php
+++ page-find.php
@@ -334,23 +334,36 @@ $the_query = new WP_Query($args);
 
 
         <!-- 検索結果表示 -->
-        <div class="result_img">
+        <div class="searcharea">
             <h2 class="title">検索結果一覧</h2>
-            <div class="result_img_wrap flex">
+            <div class="searcharea_item searcharea_flex">
                 <?php if ($the_query->have_posts()) : ?>
                 <?php while ($the_query->have_posts()) : ?>
                 <?php $the_query->the_post(); ?>
                 <a href="<?php the_permalink() ?>">
-                    <div class="result_img_card">
+                    <div class="searcharea_item_card">
                         <?php $eye_catching = get_field('eye_catching');?>
                         <?php if(!empty($eye_catching)): ?>
                         <img src="<?php the_field('eye_catching'); ?>" alt="">
                         <?php else: ?>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/text_kakko_kari.png" alt="">
                         <?php endif; ?>
-                        <p><?php the_field('name') ?></p>
+                        <p class="searcharea_item_card_title"><?php the_field('name') ?></p>
                         <?php //'ボランティアは'.the_field('recruitment'); ?>
-                        <?php echo get_the_terms($post->ID, 'area')[1]->name; ?>
+                        <p class="searcharea_item_card_title border">
+                            <?php echo get_the_terms($post->ID, 'area')[1]->name; ?>
+                        </p>
+                        <p class="searcharea_text">
+                            <?php $features = get_field('features');
+                                //40文字にする
+                                if(mb_strlen($features) > 40) {
+                                    $features = mb_substr($features,0,40);
+                                    echo $features . '・・・' ;
+                                } else {
+                                    echo $features;
+                                } 
+                            ?>
+                        </p>
                     </div>
                 </a>
                 <?php endwhile; ?>
