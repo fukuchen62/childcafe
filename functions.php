d2252cbec9cc62d8d10e2c8ac140b0997d1c71b4
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-15 16:47:23 +0900

    エリアからさがすのtitleタグの表示変更しました
    エリアからさがすのtitleタグの表示変更しました
    
    三宅

--- functions.php
+++ functions.php
@@ -13,9 +13,11 @@ add_filter( 'script_loader_src', 'remove_cssjs_ver2', 9999 );
 
 //after_setup_themeアクションフックを使用する関数をまとめる
 function my_theme_setup() {
-    add_theme_support('title-tag'); //<title>タグを出力する
     add_theme_support('post-thumbnails'); //アイキャッチ画像を使用可能にする
     add_theme_support('menus'); //カスタムメニュー機能を使用可能にする
+    if (!is_tax('area')) {
+        add_theme_support('title-tag'); //<title>タグを出力する
+    }
 }
 add_action('after_setup_theme', 'my_theme_setup');
 
