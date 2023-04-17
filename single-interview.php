b9d4124738e9b73171b8edfb578167ca0e879dae
Miyake-Nozomi <128271935+Miyake-Nozomi@users.noreply.github.com>
2023-04-17 12:11:34 +0900

    Update single-interview.php

--- single-interview.php
+++ single-interview.php
@@ -7,13 +7,20 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
 <main>
     <div class="main_inner">
         <?php get_template_part('template-parts/breadcrumb'); ?>
-        <h2 class="pickup_title_1">
+        <h2 class="pickup_title_1 pickup_title">
+            <!-- <span class="pickup_title sp">
+                <?php //the_field('title'); ?>
+            </span>
             <span class="pickup_title sp">
+                <?php //echo the_field('organizer'); ?>
+            </span> -->
+            <!-- <span class="pickup_title sp"> -->
+                <nobr>
                 <?php the_field('title'); ?>
-            </span>
-            <span class="pickup_title sp">
+                <wbr>
                 <?php echo the_field('organizer'); ?>
-            </span>
+                </nobr>
+            <!-- </span> -->
         </h2>
         <div class="pickup_single">
             <div class="pickup_top_img">
@@ -26,8 +33,7 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
                     <?php if(!empty($eye_catching)): ?>
                     <img src="<?php echo $eye_catching; ?>" alt="<?php echo $image_alt; ?>">
                     <?php else: ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage/logo_eye_catch.png"
-                        alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage/logo_eye_catch.png" alt="">
                     <?php endif; ?>
                 </div>
                 <div class="underimg text cafeinfo_flex flex">
