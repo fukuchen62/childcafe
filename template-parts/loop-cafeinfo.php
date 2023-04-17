cd95b4ca2b6431ef2904fd60c58e7810324f18f4
nozaki08 <uryahoooi89@gmail.com>
2023-04-17 14:26:02 +0900

    Update loop-cafeinfo.php
    カード修正
    
    野崎

--- template-parts/loop-cafeinfo.php
+++ template-parts/loop-cafeinfo.php
@@ -3,21 +3,21 @@ $this_terms = get_the_terms($post->ID,'area');
 ?>
 
 <a href="<?php the_permalink(); ?>">
-    <article class="area_item_card">
+    <article class="item_card">
         <?php $eye_catching = get_field('eye_catching');?>
         <?php if(!empty($eye_catching)): ?>
         <img src="<?php the_field('eye_catching'); ?>" alt="">
         <?php else: ?>
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/text_kakko_kari.png" alt="">
         <?php endif; ?>
-        <p class="area_item_card_title">
+        <p class="item_card_title">
             <?php the_field('name'); ?></p>
-        <p class="area_item_card_title border">
+        <p class="item_card_title border">
             <?php
                 echo $this_terms[1]->name;
             ?>
         </p>
-        <p class="area_text">
+        <p class="item_card_text">
             <?php
             //整形したい文字列
             if (!empty(get_field('features'))) {
