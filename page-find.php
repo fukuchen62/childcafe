a492f21a49b112b888327f1df085942740f0e318
yshizuka <128563427+yshizuka@users.noreply.github.com>
2023-04-17 16:49:28 +0900

    ボタンの位置修正
    ボタンの位置修正
    
    よしだ

--- page-find.php
+++ page-find.php
@@ -253,7 +253,9 @@ $the_query = new WP_Query($args);
                                 </label>
                                 <?php foreach ($east as $town) :  ?>
                                 <label for="<?php echo $town->slug; ?>">
-                                    <input type="checkbox" id="<?php echo $town->slug; ?>" name="area[]" value="<?php echo $town->slug; ?>" class="east_list" /><?php echo $town->name; ?>
+                                    <input type="checkbox" id="<?php echo $town->slug; ?>" name="area[]"
+                                        value="<?php echo $town->slug; ?>"
+                                        class="east_list" /><?php echo $town->name; ?>
                                 </label>
                                 <?php endforeach; ?>
                             </div>
@@ -268,7 +270,9 @@ $the_query = new WP_Query($args);
                                 </label>
                                 <?php foreach ($south as $town) :  ?>
                                 <label for="<?php echo $town->slug; ?>">
-                                    <input type="checkbox" id="<?php echo $town->slug; ?>" name="area[]" value="<?php echo $town->slug; ?>" class="south_list" /><?php echo $town->name; ?>
+                                    <input type="checkbox" id="<?php echo $town->slug; ?>" name="area[]"
+                                        value="<?php echo $town->slug; ?>"
+                                        class="south_list" /><?php echo $town->name; ?>
                                 </label>
                                 <?php endforeach; ?>
                             </div>
@@ -282,7 +286,9 @@ $the_query = new WP_Query($args);
                                 </label>
                                 <?php foreach ($west as $town) :  ?>
                                 <label for="<?php echo $town->slug; ?>">
-                                    <input type="checkbox" id="<?php echo $town->slug; ?>" name="area[]" value="<?php echo $town->slug; ?>" class="west_list" /><?php echo $town->name; ?>
+                                    <input type="checkbox" id="<?php echo $town->slug; ?>" name="area[]"
+                                        value="<?php echo $town->slug; ?>"
+                                        class="west_list" /><?php echo $town->name; ?>
                                 </label>
                                 <?php endforeach; ?>
                             </div>
@@ -318,12 +324,12 @@ $the_query = new WP_Query($args);
                     </div>
                     <!--チェックボックス欄  終了-->
                 </div>
+                <!-- ボタン -->
+                <div class="form_btns flex">
+                    <input class="submit_btn" type="submit" value="さがす" />
+                    <input class="reset_btn" type="reset" value="リセット" />
+                </div>
             </section>
-            <!-- ボタン -->
-            <div class="form_btns flex">
-                <input class="submit_btn" type="submit" value="さがす" />
-                <input class="reset_btn" type="reset" value="リセット" />
-            </div>
         </form>
 
 
@@ -363,7 +369,8 @@ $the_query = new WP_Query($args);
                 <?php endwhile; ?>
                 <?php else:?>
                 <h3>お探しのこども食堂が見つかりませんでした。</h3>
-                <img src="<?php echo get_template_directory_uri();?>/assets/images/index/notfind.png" alt="" class="searcharea_404" />
+                <img src="<?php echo get_template_directory_uri();?>/assets/images/index/notfind.png" alt=""
+                    class="searcharea_404" />
                 <?php endif;?>
                 <?php wp_reset_postdata(); ?>
             </div>
