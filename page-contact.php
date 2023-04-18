07715d6d5727c27523185ef7db75e35beb22fef9
yshizuka <128563427+yshizuka@users.noreply.github.com>
2023-04-18 17:57:29 +0900

    ボタン修正
    よしだ

--- page-contact.php
+++ page-contact.php
@@ -70,8 +70,8 @@
                     <p> なお、各こども食堂の開催日時等の運営状況や人員の都合により、返信までに時間がかかる場合があります。予めご了承ください。</p>
                 </ul>
             </div>
-            <div class="btn">
-                <a class="btn_a" href="<?php echo home_url('/cafeinfo'); ?>">各こども食堂一覧</a>
+            <div class="btn_item">
+                <a href="<?php echo home_url('/cafeinfo'); ?>">各こども食堂一覧</a>
             </div>
         </section>
         <!-- 各子供食堂 終了-->
