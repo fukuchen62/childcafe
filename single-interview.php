86af563bf244c71237b74f8c140600f6b352f2a2
nozaki08 <uryahoooi89@gmail.com>
2023-04-18 19:31:23 +0900

    修正
    修正中
    
    野崎

--- single-interview.php
+++ single-interview.php
@@ -23,44 +23,42 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
         <?php get_template_part('template-parts/breadcrumb'); ?>
         <div class="pickup_single">
             <h2 class="pickup_title_1 pickup_title">
-                <!-- <span class="pickup_title sp">
-                <?php //the_field('title'); ?>
-            </span>
-            <span class="pickup_title sp">
-                <?php //echo the_field('organizer'); ?>
-            </span> -->
-                <!-- <span class="pickup_title sp"> -->
                 <nobr>
                     <?php the_field('title'); ?>
                     <wbr>
                     <?php echo the_field('organizer'); ?>
                 </nobr>
-                <!-- </span> -->
             </h2>
-            <div class="pickup_top_img">
-                <div class="pickup_single_img">
-                    <?php
+            <div class="area_flex">
+                <div>
+                    <div class="area_flex pickupimg">
+                        <div class="pickup_top_img">
+                            <div class="pickup_single_img">
+                                <?php
                         $eye_catching = get_field('eye_catching');
                         $image_id = attachment_url_to_postid( $eye_catching );
-                        $image_alt = get_post_meta(  $image_id, '_wp_attachment_image_alt', true );
-                    ?>
-                    <?php if(!empty($eye_catching)): ?>
-                    <img src="<?php echo $eye_catching; ?>" alt="<?php echo $image_alt; ?>">
-                    <?php else: ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage/logo_eye_catch.png" alt="">
-                    <?php endif; ?>
+                        $image_alt = get_post_meta(  $image_id, '_wp_attachment_image_alt', true );?>
+                                <?php if(!empty($eye_catching)): ?>
+                                <img src="<?php echo $eye_catching; ?>" alt="<?php echo $image_alt; ?>">
+                                <?php else: ?>
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noimage/logo_eye_catch.png"
+                                    alt="">
+                                <?php endif; ?>
+                            </div>
+                            <div class="underimg text cafeinfo_flex flex">
+                                <span class="address">
+                                    <?php echo $this_terms[1]->name; ?>
+                                </span>
+                                <span>
+                                    <?php echo do_shortcode('[wp_ulike]'); ?>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pickup_single">
+                        <?php the_field('message'); ?>
+                    </div>
                 </div>
-                <div class="underimg text cafeinfo_flex flex">
-                    <span class="address">
-                        <?php echo $this_terms[1]->name; ?>
-                    </span>
-                    <span>
-                        <?php echo do_shortcode('[wp_ulike]'); ?>
-                    </span>
-                </div>
-            </div>
-            <!-- インタビュー項目一覧 -->
-            <div class="list_area area_flex">
                 <div class="area_east">
                     <h3 class="area_title">インタビュー項目一覧</h3>
                     <ul class="area_list_wrap">
@@ -73,11 +71,9 @@ $this_terms = get_the_terms($cafeinfo_id,'area');
                         <?php endforeach; ?>
                     </ul>
                 </div>
-                <!-- インタビュー部分 -->
-                <div class="pickup_single">
-                    <?php the_field('message'); ?>
-                </div>
             </div>
+            <!-- インタビュー項目一覧 -->
+
             <div class="btn_item">
                 <a href="<?php echo home_url('/cafeinfo/' . get_field('id')); ?>">食堂紹介へ</a>
             </div>
