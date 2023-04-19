4af79dea1ad90b65d2396e55a23db91112fa7cb0
nozaki08 <uryahoooi89@gmail.com>
2023-04-19 18:20:55 +0900

    footer更新
    footer更新
    
    野崎

--- footer.php
+++ footer.php
@@ -1,11 +1,19 @@
 <footer class="footer">
-    <!-- 波型の画像 -->
-    <!-- <img
-                class="footer_wave"
-                src="../assets/images/footer_wave.svg"
-                alt=""
-            /> -->
     <div class="footer_inner">
+        <div class="footer_banner">
+            <a href="https://awa-nolife.com/farmer/">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact/awa_nolife_logo-02.png"
+                    alt="あわ農ライフ">
+            </a>
+            <a href="http://yuruhenro.com/">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact/yuruhenro_logo-02.png"
+                    alt="ゆるへんろ">
+            </a>
+            <a href="https://www.tokuparknavi.com/">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact/go_go_park_logo.png"
+                    alt="とくしまGo!Go!パークなび">
+            </a>
+        </div>
         <nav class="footer_container with_girl">
             <?php
             $hoge = array(
